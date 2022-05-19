bd99fc734339ec4176d89096ad8800ada4eac9f9
Amaury <96480667+amauryrob@users.noreply.github.com>
2022-05-19 21:35:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from tkinter import *
 from tkinter.ttk import *
 
 #Propriétées de la fenêtre
-root = Tk()
-root.geometry("300x300")
-root.title("Notes Rapides")
-root.configure(bg="#e0e065")
-root.attributes('-topmost', True)
+root = Tk()                           #créer une fenêtre
+root.geometry("300x300")              #dimensions de cette fenêtre
+root.title("Notes Rapides")           #nom de la fenêtre
+root.configure(bg="#e0e065")          #arrière plan de couleur jaune
+root.attributes('-topmost', True)     #fenêtre toujours en avant plan
 
 #Zone de texte
-zonetext = Text(root, bg="#e0e065")
-zonetext.pack()
+zonetext = Text(root, bg="#e0e065")   #zone de texte
+zonetext.pack()                       #afficher la zone de texre
 
 #lancer la fenêtre
-root.mainloop()
+root.mainloop()                       #lancer le programme
